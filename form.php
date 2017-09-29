7aad6702d67ab2fceba494d9e15382daa47836cf
harisadhyaksa <32253338+harisadhyaksa@users.noreply.github.com>
2017-09-29 10:53:32 +0700

    LEVEL 1 - Checking for Form Submission

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> PHP Forms</title>
+</head>
+<body>
+
+<form action="form.php" method="post">
+<input type="text" placeholder="Enter Username"/>
+<input type="password" placeholder="Enter Password"/>
+<br/>
+<input type="submit" name="submit"/>
+</form>
+
+</body>
+</html>
